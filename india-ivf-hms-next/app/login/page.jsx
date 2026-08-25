d5a74a91acb63c5a8f37f87687072cabd1bb1ef2
ghanshyam <webdesign@indiaivf.in>
2026-08-25 13:08:02 +0530

    login page update

--- india-ivf-hms-next/app/login/page.jsx
+++ india-ivf-hms-next/app/login/page.jsx
@@ -25,16 +25,25 @@ export default function LoginPage() {
 
     try {
       // 1. JSON Payload Fetching
-      const res = await fetch('http://127.0.0.1:8000/login/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username: username.trim(),
-          password: password.trim(),
-        }),
-      });
+      //  const res = await fetch('http://127.0.0.1:8000/login/', {
+      //   method: 'POST',
+      //  headers: {
+      //    'Content-Type': 'application/json',
+      //  },
+      //  body: JSON.stringify({
+      //    username: username.trim(),
+      //    password: password.trim(),
+      //  }),
+      // });
+
+      // Post Request to Django Auth
+const res = await fetch('/api/login/', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ username, password }),
+});
 
       const data = await res.json();
 
